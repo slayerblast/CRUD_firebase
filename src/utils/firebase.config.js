79c73a98f40e6712c573e650ef497b8c94acb9ed
Slayerblast <hery_75@hotmail.fr>
2022-10-21 12:18:37 +0200

    ajout de la config firebase

---
+++ src/utils/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1U0uiw0ooY_PYF-yVFb69KDDdY8q_HMs",
+  authDomain: "crud-app-e7089.firebaseapp.com",
+  projectId: "crud-app-e7089",
+  storageBucket: "crud-app-e7089.appspot.com",
+  messagingSenderId: "336281069115",
+  appId: "1:336281069115:web:6c47aa0ee639b013331e1e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
